a4d9247debee7c1c95d3ebc5bb44adad6d1248bc
Ghata Saraiya <ghatasaraiya@gmail.com>
2020-04-23 13:45:59 +0530

    multi_rnn

---
+++ flask1/main.py
@@ -0,0 +1,37 @@
+from flask import  Flask,render_template,request
+import pickle
+import predictfunctions
+import json
+with open('./clf.pickle', 'rb') as f:
+    lsvc = pickle.load(f)
+with open('./tf.pickle', 'rb') as f:
+    tfidf_vectorizer = pickle.load(f)
+with open('multi.pickle', 'rb') as f:
+    multilabel_binarizer = pickle.load(f)
+with open('onevrest.pickle', 'rb') as f:
+    onevrest = pickle.load(f)
+
+app=Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def home():
+    return render_template("genrepredict.html")
+
+@app.route("/predict/",methods=['GET','POST'])
+def predict():
+    query = request.args.get('query')
+
+    # stuff happens here that involves data to obtain a result
+    l = predictfunctions.linearsvc(lsvc,query)
+    o = predictfunctions.multionvsrest(tfidf_vectorizer,multilabel_binarizer,onevrest,query)
+    data = {'linearsvc': l['reply'], 'onevrest': o['reply']}
+    response = data
+
+    # result = [CBReply(data[0]['query'])]
+
+    return response
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
---
+++ flask1/predictfunctions.py
@@ -0,0 +1,24 @@
+from preprocessfunctions import preprocesswithoutstem,clean_text,remove_stopwords1
+from sklearn.svm import LinearSVC
+
+def linearsvc(clf,summary):
+    summary=preprocesswithoutstem(summary)
+    y_pred=clf.predict([summary])
+    return {'reply':list(y_pred)}
+
+def multionvsrest(tf,multi,clf,summary):
+
+    return {'reply': infer_tags(tf,multi,clf,summary)}
+
+
+def infer_tags(tfidf_vectorizer,multilabel_binarizer,clf,q):
+    q = clean_text(q)
+    q = remove_stopwords1(q)
+    q_vec = tfidf_vectorizer.transform([q])
+    #q_pred = clf.predict(q_vec)
+
+    q_pred_prob = clf.predict_proba(q_vec)
+    t = 0.2505 # threshold value 0.2505 -> 0.7110468896756014
+    q_pred_new = (q_pred_prob >= t).astype(int)
+
+    return multilabel_binarizer.inverse_transform(q_pred_new)
