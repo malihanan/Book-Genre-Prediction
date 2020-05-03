57c73ee6173bc439cdcbc1e6e103c39ccb71c38e
Ghata Saraiya <ghatasaraiya@gmail.com>
2020-05-03 10:17:08 +0530

    Report Version 2, SGD classifier with Ngrams
    All changes made to report.

--- flask/main.py
+++ flask/main.py
@@ -2,6 +2,8 @@ from flask import  Flask,render_template,request
 import pickle
 import predictfunctions
 import json
+from preprocessfunctions import preprocesswithoutstem
+
 with open('./clf.pickle', 'rb') as f:
     lsvc = pickle.load(f)
 with open('./tf.pickle', 'rb') as f:
@@ -10,6 +12,8 @@ with open('multi.pickle', 'rb') as f:
     multilabel_binarizer = pickle.load(f)
 with open('onevrest.pickle', 'rb') as f:
     onevrest = pickle.load(f)
+with open('./sgd1.pickle','rb') as f:
+    sgdclf=pickle.load(f)
 
 app=Flask(__name__)
 
@@ -20,9 +24,11 @@ def home():
 @app.route("/predict/",methods=['GET','POST'])
 def predict():
     query = request.args.get('query')
-    l = predictfunctions.linearsvc(lsvc,query)
+    preprocessedq=preprocesswithoutstem(query)
+    l = predictfunctions.linearsvc(lsvc,preprocessedq)
+    s=predictfunctions.sgd(sgdclf,preprocessedq)
     o = predictfunctions.multionvsrest(tfidf_vectorizer,multilabel_binarizer,onevrest,query)
-    data = {'linearsvc': l['reply'], 'onevrest': o['reply']}
+    data = {'linearsvc': l['reply'],'sgd':s['reply'], 'onevrest': o['reply']}
     response = data
     return response
 
--- flask/predictfunctions.py
+++ flask/predictfunctions.py
@@ -2,23 +2,18 @@ from preprocessfunctions import preprocesswithoutstem,clean_text,remove_stopword
 from sklearn.svm import LinearSVC
 
 def linearsvc(clf,summary):
-    summary=preprocesswithoutstem(summary)
     y_pred=clf.predict([summary])
     return {'reply':list(y_pred)}
 
-def multionvsrest(tf,multi,clf,summary):
+def sgd(clf,summary):
+    y_pred=clf.predict([summary])
+    return {'reply':list(y_pred)}
 
-    return {'reply': infer_tags(tf,multi,clf,summary)}
-
-
-def infer_tags(tfidf_vectorizer,multilabel_binarizer,clf,q):
+def multionvsrest(tfidf_vectorizer,multilabel_binarizer,clf,q):
     q = clean_text(q)
     q = remove_stopwords1(q)
     q_vec = tfidf_vectorizer.transform([q])
-    #q_pred = clf.predict(q_vec)
-
     q_pred_prob = clf.predict_proba(q_vec)
     t = 0.2505 # threshold value 0.2505 -> 0.7110468896756014
     q_pred_new = (q_pred_prob >= t).astype(int)
-
-    return multilabel_binarizer.inverse_transform(q_pred_new)
+    return {'reply': multilabel_binarizer.inverse_transform(q_pred_new)}
